b20bf874063c3fabb9964d838e5d56c1bc5f6f4b
Sotatek-DatDang <dat.dang@sotatek.com>
2024-12-10 16:03:42 +0700

    profile-card

---
+++ profile-card/js/index.js
@@ -0,0 +1,6 @@
+// Write custom JavaScript here.
+// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const button = document.getElementsByClassName('contact');
+button[0].addEventListener('click', function() {
+	window.open('/hello', '_blank');
+ });
